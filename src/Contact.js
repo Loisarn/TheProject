d99084146c4cd82db43c1cd92795ef8adcd98035
Loisarn <bolo21mv@student.ju.se>
2022-04-07 08:00:20 +0200

    added a txt document

--- src/Contact.js
+++ src/Contact.js
@@ -3,18 +3,6 @@ import {main} from './index.js';
 export function showContact() {
     main.innerHTML = 
     `<div><h1>Contact</h1></div>
-    <p>LouiceBorg@hotmail.se</p>
+    <p><a href="mailto:LouiceBorg@hotmail.se" target="_blank">LouiceBorg@hotmail.se</a></p>
     `;
 }
-
-// export class Contact {
-
-//     constructor(fullName, email){
-//     this.fullName = fullName;
-//     this.email = email;
-//     }
-// }
-
-// let contact = new Contact("Louice Borg", "Bolo21mv@student.ju.se");
-
-// console.log(this.fullName, this.email);
--- src/Project.js
+++ src/Project.js
@@ -11,11 +11,16 @@ class Project {
 
 export function showProject() {
 
+    //const mockUp  -bild
+
     main.innerHTML = `
     <div><h1>Projektidé</h1></div>
     <div class="container">
-    <div> 
-    INSERT TEXT HERE
+    <div> <p>
+    <span>Floppy</span> är en app där man kan registrera info om till exempel lagningar på bilen, 
+    eller om man har renoverat i huset. Man kan även spara sina kvitton och få kvitton till appen.
+    <span>Floppy</span> handlar om att ha en bättre koll över lagningar/renoveringar och viktiga papper.
+    </p>
     </div>
     </div> `
 }
